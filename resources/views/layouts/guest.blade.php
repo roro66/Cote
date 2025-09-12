e508e3808fdb6f51b2747b3cb5ca96372a915f5d
rorro66 <rimb1966@gmail.com>
2025-09-12 14:17:00 -0300

    chore(branding): update seamless bill pattern and apply as login background

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,14 +15,15 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900" style="background-image: url('{{ asset('assets/img/pattern-bills.svg') }}'); background-repeat: repeat; background-size: 160px 160px;">
+            <div class="mb-4">
+                <a href="/" class="inline-block">
+                    {{-- Make the logo larger on login screen (approx 4x) --}}
+                    <x-application-logo class="w-80 h-80 fill-current text-gray-500" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white/90 dark:bg-gray-800/80 shadow-md overflow-hidden sm:rounded-lg" style="backdrop-filter: blur(6px);">
                 {{ $slot }}
             </div>
         </div>
